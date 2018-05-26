121f1cbe8de9f57dcd95b574128080152b22d112
jwakefield97 <jacobstevenwakefield@gmail.com>
2018-05-26 03:29:26 -0500

    change to jpa repos

--- src/main/java/springtemplate/dao/repository/UserRepository.java
+++ src/main/java/springtemplate/dao/repository/UserRepository.java
@@ -1,12 +1,12 @@
 package springtemplate.dao.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import springtemplate.dao.domain.User;
 
 //This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 //CRUD refers Create, Read, Update, Delete
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
 }
--- src/main/java/springtemplate/dao/repository/impl/UserRespositoryImpl.java
+++ src/main/java/springtemplate/dao/repository/impl/UserRespositoryImpl.java
@@ -1,7 +1,12 @@
 package springtemplate.dao.repository.impl;
 
+import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Repository;
 
 import springtemplate.dao.domain.User;
@@ -11,13 +16,79 @@ import springtemplate.dao.repository.UserRepository;
 public class UserRespositoryImpl implements UserRepository {
 
 	@Override
-	public <S extends User> S save(S entity) {
+	public List<User> findAll() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public <S extends User> Iterable<S> saveAll(Iterable<S> entities) {
+	public List<User> findAll(Sort sort) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<User> findAllById(Iterable<Long> ids) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends User> List<S> saveAll(Iterable<S> entities) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void flush() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public <S extends User> S saveAndFlush(S entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteInBatch(Iterable<User> entities) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteAllInBatch() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public User getOne(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends User> List<S> findAll(Example<S> example) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends User> List<S> findAll(Example<S> example, Sort sort) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Page<User> findAll(Pageable pageable) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends User> S save(S entity) {
 		// TODO Auto-generated method stub
 		return null;
 	}
@@ -32,18 +103,6 @@ public class UserRespositoryImpl implements UserRepository {
 	public boolean existsById(Long id) {
 		// TODO Auto-generated method stub
 		return false;
-	}
-
-	@Override
-	public Iterable<User> findAll() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Iterable<User> findAllById(Iterable<Long> ids) {
-		// TODO Auto-generated method stub
-		return null;
 	}
 
 	@Override
@@ -74,6 +133,30 @@ public class UserRespositoryImpl implements UserRepository {
 	public void deleteAll() {
 		// TODO Auto-generated method stub
 		
+	}
+
+	@Override
+	public <S extends User> Optional<S> findOne(Example<S> example) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends User> Page<S> findAll(Example<S> example, Pageable pageable) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends User> long count(Example<S> example) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public <S extends User> boolean exists(Example<S> example) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 }
