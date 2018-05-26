eb16dff0d16fb7a6317fe780b7a2d588d716314e
jwakefield97 <jacobstevenwakefield@gmail.com>
2018-05-26 03:11:43 -0500

    add service to HomeController

--- src/main/java/springtemplate/controllers/HomeController.java
+++ src/main/java/springtemplate/controllers/HomeController.java
@@ -1,12 +1,18 @@
 package springtemplate.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import springtemplate.dao.service.UserService;
+
 
 @Controller
 public class HomeController {
+	
+	@Autowired 
+	UserService userService;
 	
 	@RequestMapping("/")
 	public ModelAndView home(ModelAndView mav) {
