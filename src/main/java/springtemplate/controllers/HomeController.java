d993487edfa8656a3b722e0132a467855adb669d
jwakefield97 <jacobstevenwakefield@gmail.com>
2018-05-27 18:35:01 -0500

    add responsebody eample

--- src/main/java/springtemplate/controllers/HomeController.java
+++ src/main/java/springtemplate/controllers/HomeController.java
@@ -1,13 +1,16 @@
 package springtemplate.controllers;
 
 import java.sql.SQLException;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import springtemplate.dao.dao.UserDao;
+import springtemplate.dao.domain.User;
 
 
 @Controller
@@ -23,5 +26,12 @@ public class HomeController {
 		mav.addObject("users",userDao.getAll());
 		return mav;
 	}
+	
+	@RequestMapping("/getAllUsers")
+	@ResponseBody
+	public List<User> getAllUsers(){
+		return userDao.getAll();
+	}
+	
 
 }
